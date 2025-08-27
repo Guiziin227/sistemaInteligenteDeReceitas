2f0b970e52f52a8a339876ad24094f0ad0e6acbf
Guiziin227 <gh63388@gmail.com>
2025-08-27 10:55:57 -0300

    backend

---
+++ backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/SistemaDeReceitasApplication.java
@@ -0,0 +1,13 @@
+package com.github.guiziin227.sistemadereceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReceitasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeReceitasApplication.class, args);
+    }
+
+}
---
+++ backend/sistemaDeReceitas/src/test/java/com/github/guiziin227/sistemadereceitas/SistemaDeReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guiziin227.sistemadereceitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReceitasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
