b050b9dbb96cfbd3d62d1e4d81b160293824e041
Guiziin227 <gh63388@gmail.com>
2025-09-01 18:47:40 -0300

    adiciona métodos de busca por nome e email nos repositórios de usuário e papel

--- backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/repository/UserRepository.java
+++ backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/repository/UserRepository.java
@@ -4,8 +4,11 @@ import com.github.guiziin227.sistemadereceitas.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByFirstName(String username);
+    Optional<User> findByEmail(String email);
 }
