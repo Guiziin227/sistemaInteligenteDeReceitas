f3cf2300562aa70f18e743c1a38dca02b9c7fb6f
Guiziin227 <gh63388@gmail.com>
2025-09-03 12:17:09 -0300

    adiciona método para buscar ingredientes por nome normalizado

--- backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/repository/IngredientsRepository.java
+++ backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/repository/IngredientsRepository.java
@@ -4,6 +4,9 @@ import com.github.guiziin227.sistemadereceitas.entities.Ingredients;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IngredientsRepository extends JpaRepository<Ingredients, Long> {
+    Optional<Ingredients> findByNormalizedName(String normalizedName);
 }
