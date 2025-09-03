14549d13b5ce202bd68ea772d15b6c09a523afc0
Guiziin227 <gh63388@gmail.com>
2025-09-03 11:29:39 -0300

    adiciona repositórios

---
+++ backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/repository/UserIngredientsRepository.java
@@ -0,0 +1,9 @@
+package com.github.guiziin227.sistemadereceitas.repository;
+
+import com.github.guiziin227.sistemadereceitas.entities.UserIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserIngredientsRepository extends JpaRepository<UserIngredient, Long> {
+}
