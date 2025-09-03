5ad9ccff6dc8d4c3b1f99f59039576aa694090ca
Guiziin227 <gh63388@gmail.com>
2025-09-03 11:29:08 -0300

    adiciona endpoint para obter usuário por email

--- backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/controller/UserController.java
+++ backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.github.guiziin227.sistemadereceitas.controller;
 
 import com.github.guiziin227.sistemadereceitas.dtos.request.UserRequestDTO;
+import com.github.guiziin227.sistemadereceitas.dtos.response.UserResponseDTO;
 import com.github.guiziin227.sistemadereceitas.entities.User;
 import com.github.guiziin227.sistemadereceitas.service.UserService;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -37,6 +39,21 @@ public class UserController {
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<User>> getAllUsers() {
         var userResp = userService.getAllUsers();
+        return ResponseEntity.ok(userResp);
+    }
+
+    @GetMapping("/email")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<UserResponseDTO> getUserByEmail(@RequestParam String email) {
+
+        var user = userService.getUserByEmail(email);
+
+        var userResp = new UserResponseDTO(
+                user.firstName(),
+                user.email(),
+                user.role()
+        );
+
         return ResponseEntity.ok(userResp);
     }
 }
