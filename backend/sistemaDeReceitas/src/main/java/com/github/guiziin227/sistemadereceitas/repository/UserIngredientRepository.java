b5c9532e3e53c4595d691ab311bea3e9bc823549
Guiziin227 <gh63388@gmail.com>
2025-09-06 18:16:31 -0300

    add method to find UserIngredient by user and ingredient

--- backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/repository/UserIngredientRepository.java
+++ backend/sistemaDeReceitas/src/main/java/com/github/guiziin227/sistemadereceitas/repository/UserIngredientRepository.java
@@ -1,9 +1,14 @@
 package com.github.guiziin227.sistemadereceitas.repository;
 
+import com.github.guiziin227.sistemadereceitas.entities.Ingredients;
+import com.github.guiziin227.sistemadereceitas.entities.User;
 import com.github.guiziin227.sistemadereceitas.entities.UserIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserIngredientRepository extends JpaRepository<UserIngredient, Long> {
+    Optional<UserIngredient> findByUserAndIngredient(User user, Ingredients ingredient);
 }
